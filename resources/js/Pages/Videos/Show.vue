778d4d0c92ba43d77536b8206c77df118dbc7a02
Rasulov Akmal <akmal.rasulov@megafon.tj>
2023-12-17 18:39:45 +0500

    feat: add vuuse package and copy to clipboard functionality

--- resources/js/Pages/Videos/Show.vue
+++ resources/js/Pages/Videos/Show.vue
@@ -7,6 +7,7 @@ import TextInput from "@/Components/TextInput.vue";
 import Textarea from "@/Components/Textarea.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head, useForm, router } from "@inertiajs/vue3";
+import { useClipboard } from "@vueuse/core";
 
 const props = defineProps({
     video: {
@@ -14,7 +15,8 @@ const props = defineProps({
         required: true,
     },
 });
-
+const source = props.video.share_url;
+const { text, copy, copied, isSupported } = useClipboard({ source });
 const form = useForm({
     title: props.video.title,
     description: props.video.description,
@@ -32,7 +34,7 @@ const deleteVideo = () => {
         <div class="py-12">
             <div class="mx-auto mb-6 max-w-3xl sm:px-6 lg:px-8">
                 <div
-                    class="overflow-hidden bg-white p-6 shadow-sm dark:bg-gray-800 sm:rounded-lg"
+                    class="relative overflow-hidden bg-white p-6 shadow-sm dark:bg-gray-800 sm:rounded-lg"
                 >
                     <InputLabel for="share_url" value="Share Link" />
                     <input
@@ -42,6 +44,11 @@ const deleteVideo = () => {
                         readonly
                         class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
                     />
+                    <span
+                        @click="copy(source)"
+                        class="absolute right-8 cursor-pointer text-gray-900 transition-colors duration-300 hover:text-indigo-500 dark:text-white"
+                        >copy</span
+                    >
                 </div>
             </div>
             <div class="mx-auto max-w-3xl sm:px-6 lg:px-8">
