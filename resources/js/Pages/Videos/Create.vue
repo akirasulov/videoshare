5244dd54ed2da3701ef06b13e9a1aa94cdaf825b
Rasulov Akmal <akmal.rasulov@megafon.tj>
2023-12-18 22:38:38 +0500

    feat: uploading custom video

--- resources/js/Pages/Videos/Create.vue
+++ resources/js/Pages/Videos/Create.vue
@@ -34,6 +34,8 @@ const player = ref(null);
 const videoPreview = ref(null);
 const shouldCaptureAudio = ref(true);
 const currentDay = computed(() => dayjs().format("YYYY-MM-DD"));
+const file = ref(null);
+const showForm = ref(false);
 
 const startRecording = () => {
     let chunks = [];
@@ -125,6 +127,7 @@ watch(
 watch(
     () => state.blobUrl,
     (url) => {
+        console.log("url", url);
         videoPreview.value.src = url;
     },
 );
@@ -139,6 +142,14 @@ watch(
         form.description = `A video captured on ${currentDay.value}`;
     },
 );
+
+const handleFileUpload = (event) => {
+    showForm.value = true;
+    videoPreview.value.src = URL.createObjectURL(event.target.files[0]);
+    form.video = event.target.files[0];
+    form.title = currentDay.value;
+    form.description = `A video captured on ${currentDay.value}`;
+};
 </script>
 
 <template>
@@ -160,7 +171,7 @@ watch(
                 >
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         <form
-                            v-show="state.blobUrl"
+                            v-show="state.blobUrl || showForm"
                             @submit.prevent="
                                 form.post(route('videos.store'), {
                                     preserveScroll: true,
@@ -244,29 +255,77 @@ watch(
                                     >
                                 </div>
                             </div>
-                            <div
-                                v-if="!state.streamActive"
-                                class="flex items-center justify-center space-x-4"
-                            >
-                                <PrimaryButton @click="captureWebcam"
-                                    >Capture WebCam</PrimaryButton
-                                >
-                                <PrimaryButton @click="captureScreen"
-                                    >Capture Screen</PrimaryButton
-                                >
 
-                                <div class="flex items-center space-x-2">
-                                    <Checkbox
-                                        id="audio"
-                                        v-model:checked="shouldCaptureAudio"
-                                    />
-                                    <InputLabel
-                                        for="audio"
-                                        class="text-sm font-medium"
-                                        >Enable audio</InputLabel
+                            <section
+                                v-if="!state.streamActive"
+                                class="space-y-6"
+                            >
+                                <div
+                                    v-if="!showForm"
+                                    class="flex w-full items-center justify-center"
+                                >
+                                    <label
+                                        for="dropzone-file"
+                                        class="dark:hover:bg-bray-800 flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
                                     >
+                                        <div
+                                            class="flex flex-col items-center justify-center pb-6 pt-5"
+                                        >
+                                            <svg
+                                                class="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
+                                                aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 20 16"
+                                            >
+                                                <path
+                                                    stroke="currentColor"
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    stroke-width="2"
+                                                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                                                />
+                                            </svg>
+
+                                            <span class="semibold"
+                                                >Click to upload</span
+                                            >
+                                            or drag and drop
+                                        </div>
+                                        <input
+                                            @change="handleFileUpload"
+                                            ref="file"
+                                            id="dropzone-file"
+                                            type="file"
+                                            class="hidden"
+                                        />
+                                    </label>
                                 </div>
-                            </div>
+
+                                <div
+                                    v-if="!showForm"
+                                    class="flex items-center justify-center space-x-4"
+                                >
+                                    <PrimaryButton @click="captureWebcam"
+                                        >Capture WebCam</PrimaryButton
+                                    >
+                                    <PrimaryButton @click="captureScreen"
+                                        >Capture Screen</PrimaryButton
+                                    >
+
+                                    <div class="flex items-center space-x-2">
+                                        <Checkbox
+                                            id="audio"
+                                            v-model:checked="shouldCaptureAudio"
+                                        />
+                                        <InputLabel
+                                            for="audio"
+                                            class="text-sm font-medium"
+                                            >Enable audio</InputLabel
+                                        >
+                                    </div>
+                                </div>
+                            </section>
                         </div>
                     </div>
                 </div>
